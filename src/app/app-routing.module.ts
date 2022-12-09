f000c644e0f446a11f6758f771ee7eec0683765d
Job Karani <jobkarani084j@gmail.com>
2022-12-08 16:54:14 -0800

    updates contact component content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
 
@@ -8,7 +9,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
-  // { path: 'contact-us', component: ContactComponent },
+  { path: 'contact-us', component: ContactComponent },
 ];
 
 @NgModule({
