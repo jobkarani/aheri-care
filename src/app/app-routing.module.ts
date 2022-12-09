ba52c95239ee686462a5a6e7ae67ae6277ded50e
Job Karani <jobkarani084j@gmail.com>
2022-12-08 16:43:23 -0800

    updates services component content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   // { path: 'about', component: AboutComponent },
-  // { path: 'services', component: ServicesComponent },
+  { path: 'services', component: ServicesComponent },
   // { path: 'contact-us', component: ContactComponent },
 ];
 
