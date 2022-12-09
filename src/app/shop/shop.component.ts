24930d1d8aebfb9ec005a7c75ab3468485e3e175
Job Karani <jobkarani084j@gmail.com>
2022-12-10 00:17:05 +0300

    creates a get request method for products and categories

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.css']
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit {
+
+  constructor(private http:HttpClient) {}
+
+  prod:any;
+  cat:any;
+
+  ngOnInit():void{
+    this.prod = this.http.get("https://ahericaredb.up.railway.app/api_products/").subscribe(
+      data => this.prod = data
+    )
+    this.cat = this.http.get("https://ahericaredb.up.railway.app/api_categories/").subscribe(
+      data => this.cat = data
+    )
+  }
 
 }
