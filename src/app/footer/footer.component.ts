8dd6e1c50d73221a436f421716cd5345856d7d5b
Job Karani <jobkarani084j@gmail.com>
2022-12-17 14:56:16 +0300

    changes footer design

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ApiResponse2 } from '../Interfaces/api-response2';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  category:ApiResponse2[] = [];
+
+  constructor(private http:HttpClient) {
+  }
+
+  ngOnInit(){
+      this.http.get<ApiResponse2[]>("https://ahericaredbb.up.railway.app/api_categories/").subscribe(
+
+      data => {
+        this.category = data
+        console.log(this.category)
+      }
+    )
+  }
 
 }
