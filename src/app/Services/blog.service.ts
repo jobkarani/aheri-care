8440fbc4ff630f3cefd2000c172b9f21ece7fd8c
Job Karani <jobkarani084j@gmail.com>
2022-12-23 19:34:41 +0300

    creates blog service and method getblogdetails

---
+++ src/app/Services/blog.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Blogs } from '../Interfaces/blogs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogService {
+  
+  id = 0;
+
+  constructor(private http:HttpClient) { }
+
+  getBlogDetails(id:number): Observable<Blogs[]> {
+    return this.http.get<Blogs[]>('http://127.0.0.1:8000/get_blogs/' + id);
+  }
+
+
+}
--- src/app/blog-details/blog-details.component.ts
+++ src/app/blog-details/blog-details.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Blogs } from '../Interfaces/blogs';
 
 @Component({
   selector: 'app-blog-details',
   templateUrl: './blog-details.component.html',
   styleUrls: ['./blog-details.component.css']
 })
-export class BlogDetailsComponent {
+export class BlogDetailsComponent implements OnInit {
+
+  singleBlog:Blogs[] = [];
+  id = 0;
+
+  constructor(private http:HttpClient) {};
+
+  ngOnInit(): void {
+      
+  }
 
 }
