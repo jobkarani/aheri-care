6cd6045f8a56bf2d58404845dbade242a07ce6db
Job Karani <jobkarani084j@gmail.com>
2023-01-11 14:01:28 +0300

    creates contact module

---
+++ src/app/contact/contact-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ContactComponent } from './contact.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ContactComponent,
+    children: [
+        {path:"",component:ContactPageComponent},
+      ],
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ContactRoutingModule {}
