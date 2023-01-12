c43ee7db8c8de769cbff4c2e19822d434be4b2dd
Job Karani <jobkarani084j@gmail.com>
2023-01-12 14:07:10 +0300

    services page done

--- src/app/services-page/services-routing.module.ts
+++ src/app/services-page/services-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServiceDetailsComponent } from './service-details/service-details.component';
 import { ServiceListComponent } from './service-list/service-list.component';
 import { ServicesComponent } from './services.component';
 
@@ -9,7 +10,7 @@ const routes: Routes = [
     component: ServicesComponent,
     children: [
         {path:"",component:ServiceListComponent},
-        // { path: 'country-details/:id', component: CountryDetailsComponent },
+        { path: 'service-details/:id', component: ServiceDetailsComponent },
       ],
   }
 ];
--- src/app/services-page/services.module.ts
+++ src/app/services-page/services.module.ts
@@ -4,13 +4,15 @@ import { ServiceListComponent } from './service-list/service-list.component';
 import { ServicePageRoutingModule } from './services-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { ServicesComponent } from './services.component';
+import { ServiceDetailsComponent } from './service-details/service-details.component';
 
 
 
 @NgModule({
   declarations: [
     ServiceListComponent,
-    ServicesComponent
+    ServicesComponent,
+    ServiceDetailsComponent
   ],
   imports: [
     CommonModule,
