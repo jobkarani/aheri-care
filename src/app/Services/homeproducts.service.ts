cce2f9b41058e5aba5de2fa0909cec684d122a93
Job Karani <jobkarani084j@gmail.com>
2023-01-15 01:21:52 +0300

    creates homeproducts service

---
+++ src/app/Services/homeproducts.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { ApiResponse } from '../Interfaces/api-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeproductsService {
+
+  constructor(private http: HttpClient) {}
+
+  getProductsToHome() {
+    return this.http.get<ApiResponse[]>('https://ahericaredbb.up.railway.app/api_products/').pipe(
+      map(products => products.slice(-4))
+    );
+  }
+}
