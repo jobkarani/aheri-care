38ff7d53fd51107fc4f0e5d594c07bc445153c76
Job Karani <jobkarani084j@gmail.com>
2023-01-15 03:08:28 +0300

    consumes the service endpoint on footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Service } from 'src/app/Interfaces/service';
 import { ApiResponse2 } from '../../Interfaces/api-response2';
 
 @Component({
@@ -10,18 +11,22 @@ import { ApiResponse2 } from '../../Interfaces/api-response2';
 export class FooterComponent implements OnInit {
 
   category:ApiResponse2[] = [];
+  service:Service[] = [];
 
   constructor(private http:HttpClient) {
   }
 
   ngOnInit(){
-      this.http.get<ApiResponse2[]>("https://ahericaredbb.up.railway.app/api_categories/").subscribe(
+    this.http.get<ApiResponse2[]>("https://ahericaredbb.up.railway.app/api_categories/").subscribe(
+    data => {
+      this.category = data
+      console.log(this.category)
+    });
 
-      data => {
-        this.category = data
-        console.log(this.category)
-      }
-    )
+    this.http.get<Service[]>('https://ahericaredbb.up.railway.app/get_services/').subscribe(data =>{
+      this.service = data;
+      console.log(data);
+    })
   }
 
 }
