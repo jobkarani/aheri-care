483c38ad1376ecd909704d2d658066a6ef11b62b
Job Karani <jobkarani084j@gmail.com>
2023-01-17 12:40:09 +0300

    whatsapp link

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,23 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit{
+  
   constructor() { }
 
   ngOnInit(): void {
     const hamburger = document.querySelector(".hamburger");
     const navMenu = document.querySelector(".nav-menus");
     const navLink = document.querySelectorAll(".nav-links");
-
+  
     const mobileMenu = () => {
       hamburger?.classList.toggle("active")
       navMenu?.classList.toggle("active")
     }
-
+  
     const closeMenu = () => {
       hamburger?.classList.remove("active")
       navMenu?.classList.remove("active")
     }
-
+  
     hamburger?.addEventListener("click" , mobileMenu)
     navLink.forEach((l) => l.addEventListener("click",closeMenu))
   }  
