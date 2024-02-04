9e4271959fcffaa44166eac653503b08c6b51b00
Job Karani <jobkarani084j@gmail.com>
2024-02-04 05:54:08 +0300

    feat: home services and blogs fix

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+
   showMobileMenu: boolean = false;
 
   toggleMobileMenu() {
