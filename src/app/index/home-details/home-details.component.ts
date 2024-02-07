c7ca5475971e826ce453f58068d984c05e485e06
Job Karani <jobkarani084j@gmail.com>
2024-02-07 22:10:51 +0300

    fix: carousel homepage

--- src/app/index/home-details/home-details.component.ts
+++ src/app/index/home-details/home-details.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Blogs } from 'src/app/Interfaces/blogs';
 import { Service } from 'src/app/Interfaces/service';
 import { LatestblogService } from 'src/app/Services/latestblog.service';
+import { interval, Subscription } from 'rxjs';
+
+interface Image {
+  src: string;
+  alt: string;
+}
 
 @Component({
   selector: 'app-home-details',
   templateUrl: './home-details.component.html',
   styleUrls: ['./home-details.component.css']
 })
-export class HomeDetailsComponent implements OnInit {
+export class HomeDetailsComponent implements OnInit, OnDestroy {
   
   countTo:any;
   
@@ -16,18 +22,63 @@ export class HomeDetailsComponent implements OnInit {
 
   latestBlogs: Blogs[] = [];
   homeServices: Service[] = [];
+  images: Image[] = [
+    { src: './assets/Assets/nurse.webp', alt: 'Image 1 description' },
+    { src: './assets/Assets/slider2.jpeg', alt: 'Image 2 description' },
+    { src: './assets/Assets/slider4.jpeg', alt: 'Image 3 description' },
+    { src: './assets/Assets/slider3.jpeg', alt: 'Image 4 description' },
+    { src: './assets/Assets/community.jpeg', alt: 'Image 4 description' }
+  ];
+  currentSlideIndex = 0;
+  private intervalSubscription: Subscription | null = null;  
+  paused = false;
 
   ngOnInit(): void {
+
+    this.startAutoplay();
 
     this.latestblogService.getLatestBlogs().subscribe(blogs => {
       this.latestBlogs = blogs;
     });
 
-    // Get the currently active carousel item (remove "hidden" class)
-    const activeItem = document.querySelector('.carousel-item:not(.hidden)') as HTMLElement;
+  }
+  
+  ngOnDestroy() {
+    this.stopAutoplay();
+  }
+  nextSlide() {
+    if (!this.paused) {
+      this.currentSlideIndex = (this.currentSlideIndex + 1) % this.images.length;
+    }
+  }
 
-    if (activeItem) {
-      activeItem.classList.remove('hidden');
+  prevSlide() {
+    if (!this.paused) {
+      this.currentSlideIndex =
+        (this.currentSlideIndex - 1 + this.images.length) % this.images.length;
+    }
+  }
+  
+  startAutoplay() {
+    if (!this.intervalSubscription) {
+      this.intervalSubscription = interval(3000) 
+        .subscribe(() => this.nextSlide());
+    }
+  }
+
+  stopAutoplay() {
+    if (this.intervalSubscription) {
+      this.intervalSubscription.unsubscribe();
+      this.intervalSubscription = null;
+    }
+  }
+
+  pausePlay() {
+    this.paused = !this.paused;
+    if (this.paused) {
+      this.stopAutoplay();
+    } else {
+      this.startAutoplay();
     }
   }
 }
